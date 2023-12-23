a64bb98aadd39b08d78672b4c5b77c9b5dc76fb2
Seung Hwan Baek <ghks9936@naver.com>
2023-12-23 10:58:10 +0900

    15654

---
+++ 백준/백트래킹/15657.cpp
@@ -0,0 +1,54 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int num, len;
+
+map<int, bool> check;
+
+void back(vector<int>& data, int start)
+{
+    if (data.size() == len)
+    {
+        for_each(data.begin(), data.end(), [](int d) {
+            cout << d << ' ';
+         });
+        cout << '\n';
+        return;
+    }
+  
+
+    for (auto& p : check)
+    {
+        //if (p.second)
+        //    continue;
+        if (p.first < start)
+            continue;
+        data.push_back(p.first);
+        p.second = true;
+        back(data, p.first);
+        data.erase(data.end() - 1);
+        p.second = false;
+    }
+
+}
+
+int main() {
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cin >> num >> len;
+    
+    for (int i = 0; i <num; ++i)
+    {
+        int a;
+        cin >> a;
+        check.insert({a, false});
+    }
+
+    vector<int> data;
+
+    back(data, -1);
+
+    return 0;
+}
